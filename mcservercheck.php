e5e7b2aa81ecc16b6e3248f654c176087f5fc52d
Robert Lane <jinto@SilentBeast.(none)>
2013-03-29 08:49:10 -0700

    Added Functional ShortCode
    Added the main function to inject shortcode.

--- mcservercheck.php
+++ mcservercheck.php
@@ -13,13 +13,54 @@ Author URI: http://profiles.wordpress.org/robertlane
 */
 
 include_once('includes/server_check_engine.php');
-$server = new MCServerEngine("mcserver.geekgamer.tv", 25565);
+/*
+add_shortcode( 'yourmom', 'yourmomfoo' );
+function yourmomfoo() {
+return "<h3>Your Mom!</h3>";
+}
+*/
 
+add_shortcode( 'mcservercheck', 'mcserver_shortcode' );
+
+function mcserver_shortcode( $atts, $content = 'Minecraft Server' ) {
+
+extract( shortcode_atts( array(
+   'domain' => 'minecraft.example.com',
+   'port' => 25565,
+   ), $atts ) );
+
+$server = new MCServerEngine( $domain, $port );
+
+if ($server->online == false) {
+  return '<div class="mcserver_offline">Down</down>';
+}
+
+ob_start();
+?>
+<div class="mcserver_online" style="background-color:cyan">
+  <p class="name"><?php echo $content; ?></p>
+  <p class="domain"><?php echo $domain; ?></p>
+  <p class="players"><?php echo "Current Players: ".$server->online_players."/".$server->max_players; ?></p>
+  <p class="motd"><?php echo $server->motd; ?></p>
+</div>
+
+<?php
+$layout = ob_get_contents();
+ob_end_clean();
+return $layout;
+}
+
+
+/*
 class MCServer_Main {
 
 public function MCServer_Main() {
-add_action('admin_notices',  array( $this, 'my_admin_notice') );
+//add_action('admin_notices',  array( $this, 'my_admin_notice') );
+
+
 }
+
+
 
 static function my_admin_notice(){
 global $server;
@@ -35,6 +76,10 @@ ob_flush();
 
 }
 
+
+
 } // Closes the Class
 $MCServerMain = new MCServer_Main;
+*/
+
 ?>
